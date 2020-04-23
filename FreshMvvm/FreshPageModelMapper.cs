2068924d3d756b54a4402691cce3e81285d39f8b
Emil Alipiev <emil_alipiev@hotmail.com>
2020-04-24 01:05:52 +0200

    custom namespace for freshpagemodelmapper

--- FreshMvvm/FreshPageModelMapper.cs
+++ FreshMvvm/FreshPageModelMapper.cs
@@ -4,12 +4,35 @@ namespace FreshMvvm
 {
     public class FreshPageModelMapper : IFreshPageModelMapper
     {
+        readonly string pageNamespace;
+        readonly string pageAssemblyName; 
+
+        public FreshPageModelMapper(string pageNamespace = null, string pageAssemblyName = null)
+        {
+            this.pageNamespace = pageNamespace;
+            this.pageAssemblyName = pageAssemblyName;
+        }
+
         public string GetPageTypeName(Type pageModelType)
         {
-            return pageModelType.AssemblyQualifiedName
-                .Replace ("PageModel", "Page")
-                .Replace ("ViewModel", "Page");
+            var assemblyQualifiedName = pageModelType.AssemblyQualifiedName;
+
+            // Replace Namespace
+            if (pageNamespace != null)
+                assemblyQualifiedName = assemblyQualifiedName.Replace(pageModelType.Namespace, pageNamespace);
+
+            // Replace Assembly
+            if (pageAssemblyName != null)
+                assemblyQualifiedName = assemblyQualifiedName.Replace(pageModelType.Assembly.ToString(), pageAssemblyName);
+
+            // Replace "Model"
+            assemblyQualifiedName = assemblyQualifiedName
+                .Replace("PageModel", "Page")
+                .Replace("ViewModel", "Page");
+
+            return assemblyQualifiedName;
         }
+
     }
 }
 
