5bfb71ef03127041bfa1f1514fdf6286f447977e
Emil Alipiev <emil_alipiev@hotmail.com>
2021-02-27 23:06:19 +0100

    testapp added

---
+++ TestApp/TestApp/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FreshMvvm;
+
+namespace TestApp
+{
+    public partial class App : Xamarin.Forms.Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            LoadMasterDetail();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+        public class NavigationContainerNames
+        {
+            public const string AuthenticationContainer = "AuthenticationContainer";
+            public const string IntroContainer = "IntroContainer";
+            public const string DefaultNavigationServiceName = "DefaultNavigationServiceName";
+        }
+
+        public void LoadMasterDetail()
+        {
+
+            var masterDetailNav = new FreshMasterDetailNavigationContainer(NavigationContainerNames.DefaultNavigationServiceName);
+
+            masterDetailNav.Init("Menu", "Menu.png");
+            masterDetailNav.AddPage<MainPageModel>("Dashboard", null);
+
+            MainPage = masterDetailNav;
+        }
+    }
+}
---
+++ TestApp/TestApp/MainPageModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FreshMvvm;
+
+namespace TestApp
+{
+   public class MainPageModel: FreshBasePageModel
+    {
+
+    }
+}
